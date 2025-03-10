05b2d34e70f0e4b2df6c9708657956429716ec05
whrreynolds <whr@att.net>
2025-03-10 14:58:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 helpful_links = [
     "https://docs.streamlit.io",
